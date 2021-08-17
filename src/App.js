77d4646fb8f70bbb1020305f3378d4c89d98bec0
Arindam-7 <msaha6368@gmail.com>
2021-08-17 17:51:20 +0530

    added timeline

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Timeline from './components/Timeline';
 
 function App() {
   return (
     <>
       <Navbar /> 
       <Header />
+      <Timeline />
     </>
   );
 }
