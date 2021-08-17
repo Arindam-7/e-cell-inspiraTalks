f4af8f27ec331c89e3a9722120199e0eee8fa883
Arindam-7 <msaha6368@gmail.com>
2021-08-17 09:40:40 +0530

    navbar and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import mic from '../mic.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-success">
-        <a className="navbar-brand" href="/">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
-            </li>
-          </ul>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className='container'>
+            <a className="navbar-brand" href="/"><img className="logo" src={mic} alt="logo" /><span className="logoFont">InspiraTalks</span></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <FontAwesomeIcon icon={faBars} style={{color: "blue"}}/>
+            </button>
+          
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Link 1</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Link 2</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Link 3</a>
+                </li>
+              </ul>
+            </div>
+          </div>
       </nav>
     )
 }
