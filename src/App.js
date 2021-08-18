043fae1cae5fea813816dc0086f3528d8ce6ef76
achak47 <81907012+achak47@users.noreply.github.com>
2021-08-18 12:02:32 +0530

    Update App.js
    Scroll option . I have removed the navbar.js and instead copied the code in app.js for making the auto scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
+import React,{Component} from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
 import Gallery from './components/Gallery';
 import Sponsor from './components/Sponsor';
+import mic from './mic.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
-function App() {
-  return (
+class App extends React.Component {
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function(to, element) {
+      console.log('begin', arguments);
+    }) ;
+    Events.scrollEvent.register('end', function(to, element) {
+      console.log('end', arguments);
+    });
+
+    scrollSpy.update();
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+  scrollToBottom() {
+    scroll.scrollToBottom();
+  }
+  scrollToTimeline() {
+    scroller.scrollTo('time', {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: 50 // Scrolls to element + 50 pixels down the page
+    })
+  }
+  scrollToGallery(){
+    scroller.scrollTo('gallery', {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: 50 // Scrolls to element + 50 pixels down the page
+    })
+  }
+  scrollMore() {
+    scroll.scrollMore(100);
+  }
+  handleSetActive(to) {
+    console.log(to);
+  }
+  render(){
+    return (
     <>
-      <Navbar /> 
+      <nav className="navbar sticky-top navbar-expand-lg navbar-light">
+          <div className='container'>
+            <a className="navbar-brand" href="/"><img className="logo" src={mic} alt="logo" /><span className="logoFont">Inspira Talks</span></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <FontAwesomeIcon icon={faBars} style={{color: "blue"}}/>
+            </button>
+          
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link" onClick={this.scrollToTop}>Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" onClick={this.scrollToTimeline}>Timeline</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" onClick={this.scrollToGallery}>Gallery</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" onClick={this.scrollToBottom}>Sponsors</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+      </nav>
       <Header />
+      <Element name="time">
       <Timeline />
+      </Element>
+      <Element name="gallery">
       <Gallery />
+      </Element>
       <Sponsor />
     </>
-  );
+  );}
 }
 
 export default App;
